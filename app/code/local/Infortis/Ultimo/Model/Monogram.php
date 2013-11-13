93c6c073f9d57a0c8bdecca7f628736a553d43f2
Jan M <jan@websolutionsnyc.com>
2013-11-13 17:25:38 +0100

    adding support for grouped products with monograms

--- app/code/local/Infortis/Ultimo/Model/Monogram.php
+++ app/code/local/Infortis/Ultimo/Model/Monogram.php
@@ -14,27 +14,60 @@ class Infortis_Ultimo_Model_Monogram extends Mage_Core_Model_Abstract
             $type = $request->getParam('monogram-type', '');
             $color = $request->getParam('monogram-color', '');
             $initials = $request->getParam('monogram-initials', '');
-  
-            $item->setData('monogram-type', $type);
-            $item->setData('monogram-color', $color);
-            $item->setData('monogram-initials', $initials);
-            $item->setData('monogram-check', $check);
-            $item->getQuote()->save();
+      	    $check = $request->getParam('monogram-check','');
+            $monogramPrice = 16.00;
 
-            $monogramPrice = 16;
+            if($product->getTypeId()=='simple'){
+                $item->setData('monogram-type', $type);
+                $item->setData('monogram-color', $color);
+                $item->setData('monogram-initials', $initials);
+                $item->setData('monogram-check', $check);
+ 		$item->getQuote()->save();
 
-            $monogramCustomPrice =
+                $monogramCustomPrice =
                 (float)Mage::getResourceModel('catalog/product')
                     ->getAttributeRawValue($product->getId(), 'monogram_custom_price', $item->getStoreId());
-            if($monogramCustomPrice) {
-                $monogramPrice = $monogramCustomPrice;
-            }
-            $specialPrice = (float)$product->getFinalPrice() + $monogramPrice;
+                if($monogramCustomPrice) {
+                    $monogramPrice = $monogramCustomPrice;
+                }
 
-            $item->setCustomPrice($specialPrice);
-            $item->setOriginalCustomPrice($specialPrice);
-            $item->getProduct()->setIsSuperMode(true);
-            $item->save();
+                $specialPrice = (float)$product->getFinalPrice() + $monogramPrice;
+                $item->setCustomPrice($specialPrice);
+                $item->setOriginalCustomPrice($specialPrice);
+                $item->getProduct()->setIsSuperMode(true);
+                $item->save();
+
+            } elseif($product->getTypeId()=='grouped') {
+		$groupedProducts = $product->getTypeInstance(true)->getAssociatedProducts($product);
+		$groupedArray = array();
+		foreach($groupedProducts as $gProduct){
+		    $groupedArray[] = $gProduct->getId();
+		}
+		
+		foreach($item->getQuote()->getAllItems() as $qitem){
+                    if (in_array($qitem->getProductId(),$groupedArray) && $qitem->getData('monogram-type')==''){
+
+                    $monogramCustomPrice =
+                        (float)Mage::getResourceModel('catalog/product')
+                        ->getAttributeRawValue($qitem->getProductId(), 'monogram_custom_price', $qitem->getStoreId());
+                    if($monogramCustomPrice) {
+                        $monogramPrice = $monogramCustomPrice;
+                    }
+
+                    $specialPrice = (float)$qitem->getProduct()->getFinalPrice() + $monogramPrice;
+                    $qitem->setCustomPrice($specialPrice);
+                    $qitem->setOriginalCustomPrice($specialPrice);
+                    $qitem->getProduct()->setIsSuperMode(true);
+
+                        $qitem->setData('monogram-type', $type);
+                        $qitem->setData('monogram-color', $color);
+                        $qitem->setData('monogram-initials', $initials);
+                        $qitem->setData('monogram-check', $check)->save();
+		    } 
+		}
+            }
+
+            $item->getQuote()->save();
 
         endif;
 
