e09153470a7fbe8cf979b549ac1a9f53c2c6c1d6
Jan Myszkier <janmyszkier@gmail.com>
2013-11-18 19:13:11 +0100

    code cleanup

--- app/code/local/Wsnyc/WashingServiceCart/controllers/CartController.php
+++ app/code/local/Wsnyc/WashingServiceCart/controllers/CartController.php
@@ -1,26 +1,31 @@
-<?php 
+<?php
+
 class Wsnyc_WashingServiceCart_CartController 
-    extends Mage_Core_Controller_Front_Action
-{
-    public function addAction(){
- 
-    $cart = Mage::getModel('checkout/cart');
-    $postdata = $this->getRequest()->getPost();
-    
-    /*prepare array of product ids and quantities out of request params*/
-    foreach($postdata as $key => $qty){
-        $parts = explode('_',$key);
-        if ($parts[0]=='item' && $parts[2]=='qty'){
-            $productQty[$parts[1]] = $qty;
+extends Mage_Core_Controller_Front_Action {
+
+    public function addAction() 
+    {
+
+        $cart = Mage::getModel('checkout/cart');
+        $postdata = $this->getRequest()->getPost();
+
+        /* prepare array of product ids and quantities out of request params */
+        foreach ($postdata as $key => $qty) {
+            $parts = explode('_', $key);
+            if ($parts[0] == 'item' && $parts[2] == 'qty') {
+                $productQty[$parts[1]] = $qty;
+            }
         }
+
+        foreach ($productQty as $productId => $qty) {
+            $product = Mage::getModel('catalog/product')->load($productId);
+            $cart->addProduct($product, $qty);
+        }
+        $cart->save();
+
+        Mage::getSingleton('checkout/session')->setCartWasUpdated(true);
+        header("Status: 301");
+        header("Location: http://www.mywebsite.com");
     }
-    
-    foreach($productQty as $productId => $qty){
-        $product = Mage::getModel('catalog/product')->load($productId);
-        $cart->addProduct($product, $qty);
-    }
-    $cart->save();
-    
-    Mage::getSingleton('checkout/session')->setCartWasUpdated(true);
-    }
+
 }
