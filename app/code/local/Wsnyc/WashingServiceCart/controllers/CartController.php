32bfbe17f6f1dfe4965572a8d343389b6e0eda55
Jan Myszkier <janmyszkier@gmail.com>
2013-11-18 19:03:38 +0100

    fixes in washingservice cart

--- app/code/local/Wsnyc/WashingServiceCart/controllers/CartController.php
+++ app/code/local/Wsnyc/WashingServiceCart/controllers/CartController.php
@@ -3,6 +3,24 @@ class Wsnyc_WashingServiceCart_CartController
     extends Mage_Core_Controller_Front_Action
 {
     public function addAction(){
-       
+ 
+    $cart = Mage::getModel('checkout/cart');
+    $postdata = $this->getRequest()->getPost();
+    
+    /*prepare array of product ids and quantities out of request params*/
+    foreach($postdata as $key => $qty){
+        $parts = explode('_',$key);
+        if ($parts[0]=='item' && $parts[2]=='qty'){
+            $productQty[$parts[1]] = $qty;
+        }
+    }
+    
+    foreach($productQty as $productId => $qty){
+        $product = Mage::getModel('catalog/product')->load($productId);
+        $cart->addProduct($product, $qty);
+    }
+    $cart->save();
+    
+    Mage::getSingleton('checkout/session')->setCartWasUpdated(true);
     }
 }
