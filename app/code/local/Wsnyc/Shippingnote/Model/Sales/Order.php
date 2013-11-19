61e6a0c3131835fe535db5443cb516282bdfdc4a
Jan Myszkier <janmyszkier@gmail.com>
2013-11-19 23:30:49 +0100

    adding sales_order rewrite to allow new fields in email templates

---
+++ app/code/local/Wsnyc/Shippingnote/Model/Sales/Order.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Adds two new methods to be used in email templates
+ */
+class Wsnyc_Shippingnote_Model_Sales_Order extends Mage_Sales_Model_Order {
+    
+    public function getGiftMessageText() {
+        $giftMessageId = parent::getGiftMessageId();
+        return Mage::getModel('giftmessage/message')->load($giftMessageId)->getMessage();
+    }
+    
+    public function getGiftwrapBoxType(){
+        $commentInfo = parent::getOnestepcheckoutOrderComment();
+        $parts = explode(' : ',$commentInfo);
+        if(isset($parts[1])){
+            return strip_tags($parts[1]);
+        }
+        return '';
+    }
+}
