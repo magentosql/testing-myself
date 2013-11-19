00f43747efc6d39ba3d22dbe071776ffed55dc86
Jan Myszkier <janmyszkier@gmail.com>
2013-11-19 18:39:32 +0100

    adding observer, removing quote column addition, adding info in the template of order view

--- app/code/local/Wsnyc/Shippingnote/sql/wsnyc_shippingnote_setup/mysql4-upgrade-0.1.1-0.1.2.php
+++ app/code/local/Wsnyc/Shippingnote/sql/wsnyc_shippingnote_setup/mysql4-upgrade-0.1.1-0.1.2.php
@@ -4,6 +4,5 @@ $installer = new Mage_Sales_Model_Mysql4_Setup('sales_setup');
 $installer->startSetup();
 
 $installer->addAttribute('order', 'signature_required', array('type' => 'int','label' => 'Is signature required?'));
-$installer->addAttribute('quote', 'signature_required', array('type' => 'int','label' => 'Is signature required?'));
 
 $installer->endSetup();
