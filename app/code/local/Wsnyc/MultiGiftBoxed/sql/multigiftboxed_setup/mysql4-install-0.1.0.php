df344af267493883b538010dbb13e3022f8ef168
Radek Toczek <rad.toc@gmail.com>
2013-12-05 11:17:00 +0100

    ticket 103 multi gift boxes

---
+++ app/code/local/Wsnyc/MultiGiftBoxed/sql/multigiftboxed_setup/mysql4-install-0.1.0.php
@@ -0,0 +1,20 @@
+<?php
+$installer = $this;
+$installer->startSetup();
+
+$installer->run("
+		ALTER TABLE {$this->getTable('sales_flat_order')} 
+			ADD `gift_boxed_separately` TINYINT(1) DEFAULT 0;
+
+		ALTER TABLE {$this->getTable('sales_flat_order_item')}
+		    ADD `gift_boxed` TINYINT(1) DEFAULT 0;
+	");
+
+$installer->run("
+        UPDATE {$this->getTable('sales_flat_order_item')} AS `oi`
+            JOIN {$this->getTable('sales_flat_order')} AS `o`
+                ON oi.order_id=o.entity_id
+            SET `oi`.`gift_boxed`=1
+            WHERE `o`.`onestepcheckout_giftwrap_amount`>0
+    ");
+$installer->endSetup(); 
