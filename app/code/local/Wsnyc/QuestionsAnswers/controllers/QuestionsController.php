5d35a23402335d0a76ad5459da3bb33cb26fec04
Jan Myszkier <jan@websolutionsnyc.com>
2013-12-20 07:42:40 +0100

    added search and ask question forms

--- app/code/local/Wsnyc/QuestionsAnswers/controllers/QuestionsController.php
+++ app/code/local/Wsnyc/QuestionsAnswers/controllers/QuestionsController.php
@@ -11,4 +11,32 @@ class Wsnyc_QuestionsAnswers_QuestionsController
         $this->renderLayout();
 
     }
+
+    public function askAction(){
+
+        $request = $this->getRequest();
+        if($request->isPost()){
+            //Mage::getSingleton('core/session')->addMessage('some message');
+
+            $questionText = $request->getParam('ask_title').PHP_EOL.$request->getParam('ask_text');
+            $question = Mage::getModel('wsnyc_questionsanswers/question');
+
+            try{
+                $question->setQuestionText($questionText)->save();
+                Mage::getSingleton('core/session')->addError($this->__('Your question has been successfully sent'));
+            } catch(Exception $e) {
+                Mage::getSingleton('core/session')->addSuccess($this->__('There was an error sending the question, please try again later'));
+            }
+
+
+        }
+
+        $this->loadLayout();
+        $this->renderLayout();
+    }
+
+    public function searchAction(){
+        $this->loadLayout();
+        $this->renderLayout();
+    }
 }
