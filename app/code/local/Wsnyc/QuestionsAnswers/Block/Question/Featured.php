01f626890131dc8a3506f91b320f8d601073860e
Jan Myszkier <jan@websolutionsnyc.com>
2014-01-13 06:16:58 +0100

    fixing links in featured block

--- app/code/local/Wsnyc/QuestionsAnswers/Block/Question/Featured.php
+++ app/code/local/Wsnyc/QuestionsAnswers/Block/Question/Featured.php
@@ -8,21 +8,9 @@ class Wsnyc_QuestionsAnswers_Block_Question_Featured
         $collection = Mage::getResourceModel('wsnyc_questionsanswers/question_collection');
         $collection->addFieldToFilter('published',array('eq'=>'1'));
         $collection->addFieldToFilter('featured',array('eq'=>'1'));
+        $collection->clear()->setPageSize(5)->load();
         $this->setQuestionCollection($collection);
         unset($collection);
     }
 
-
-    public function getSlideshowCfg()
-    {
-        $h = Mage::helper('wsnyc_homepagebanner');
-
-        $cfg = array();
-        $cfg['navigation']			= $h->getCfg('general/navigation');
-        $cfg['slideSpeed']		= $h->getCfg('general/slideSpeed');
-        $cfg['paginationSpeed']		= $h->getCfg('general/paginationSpeed');
-        $cfg['singleItem']		= $h->getCfg('general/singleItem');
-
-        return $cfg;
-    }
 }
