030de777ecb1d99c256551ce2186ed4892a8a78e
Jan Myszkier <jan@websolutionsnyc.com>
2014-01-13 05:49:07 +0100

    changing black menu to show sibling categories

--- app/code/local/Wsnyc/QuestionsAnswers/Block/Question.php
+++ app/code/local/Wsnyc/QuestionsAnswers/Block/Question.php
@@ -9,11 +9,15 @@ class Wsnyc_QuestionsAnswers_Block_Question
         $collection->addFieldToFilter('category_id',array('eq'=>Mage::app()->getRequest()->getParam('id')));
         $collection->addFieldToFilter('published',array('eq'=>'1'));
         $this->setQuestionCollection($collection);
+
+        $questionParent = $collection->getFirstItem()->getCategoryId();
         unset($collection);
 
+        $category = Mage::getModel('wsnyc_questionsanswers/category')->load($questionParent);
+
         $collection = Mage::getResourceModel('wsnyc_questionsanswers/category_collection');
-        $collection->addFieldToFilter('parent_id',array('eq'=>0));
-        $this->setMainCategoryCollection($collection);
+        $collection->addFieldToFilter('parent_id',array('eq'=>$category->getParentId()));
+        $this->setSiblingCategoryCollection($collection);
         unset($collection);
     }
 }
