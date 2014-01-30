18c0577c6f80aef3777491a93a7e82ac5b2e5c04
Radek Toczek <rad.toc@gmail.com>
2014-01-30 09:04:24 +0100

    mass action and delete

--- app/code/local/Wsnyc/Homepagebanner/controllers/Adminhtml/BannerController.php
+++ app/code/local/Wsnyc/Homepagebanner/controllers/Adminhtml/BannerController.php
@@ -153,4 +153,82 @@ class Wsnyc_Homepagebanner_Adminhtml_BannerController
         Mage::getSingleton('adminhtml/session')->addError(Mage::helper('wsnyc_homepagebanner')->__('Unable to find item to save'));
         $this->_redirect('*/*/');
     }
+
+    public function deleteAction() {
+        if ($this->getRequest()->getParam('id') > 0) {
+            try {
+                $model = Mage::getModel('wsnyc_homepagebanner/banner');
+
+                $model->setId($this->getRequest()->getParam('id'))
+                    ->delete();
+
+                Mage::getSingleton('adminhtml/session')->addSuccess(Mage::helper('adminhtml')->__('Item was successfully deleted'));
+                $this->_redirect('*/*/');
+            } catch (Exception $e) {
+                Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
+                $this->_redirect('*/*/edit', array('id' => $this->getRequest()->getParam('id')));
+            }
+        }
+        $this->_redirect('*/*/');
+    }
+
+    public function massDeleteAction() {
+        $ids = $this->getRequest()->getParam('wsnyc_homepagebanner');
+        if (!is_array($ids)) {
+            Mage::getSingleton('adminhtml/session')->addError(Mage::helper('adminhtml')->__('Please select item(s)'));
+        } else {
+            try {
+                foreach ($ids as $id) {
+                    $bannerslider = Mage::getModel('wsnyc_homepagebanner/banner')->load($id);
+                    $bannerslider->delete();
+                }
+                Mage::getSingleton('adminhtml/session')->addSuccess(
+                    Mage::helper('adminhtml')->__(
+                        'Total of %d record(s) were successfully deleted', count($id)
+                    )
+                );
+            } catch (Exception $e) {
+                Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
+            }
+        }
+        $this->_redirect('*/*/index');
+    }
+
+    public function massStatusAction() {
+        $ids = $this->getRequest()->getParam('wsnyc_homepagebanner');
+        if (!is_array($ids)) {
+            Mage::getSingleton('adminhtml/session')->addError($this->__('Please select item(s)'));
+        } else {
+            try {
+                foreach ($ids as $id) {
+                    $bannerslider = Mage::getSingleton('wsnyc_homepagebanner/banner')
+                        ->load($id)
+                        ->setStatus($this->getRequest()->getParam('status'))
+                        ->setIsMassupdate(true)
+                        ->save();
+                }
+                $this->_getSession()->addSuccess(
+                    $this->__('Total of %d record(s) were successfully updated', count($ids))
+                );
+            } catch (Exception $e) {
+                $this->_getSession()->addError($e->getMessage());
+            }
+        }
+        $this->_redirect('*/*/index');
+    }
+
+    protected function _sendUploadResponse($fileName, $content, $contentType = 'application/octet-stream') {
+        $response = $this->getResponse();
+        $response->setHeader('HTTP/1.1 200 OK', '');
+        $response->setHeader('Pragma', 'public', true);
+        $response->setHeader('Cache-Control', 'must-revalidate, post-check=0, pre-check=0', true);
+        $response->setHeader('Content-Disposition', 'attachment; filename=' . $fileName);
+        $response->setHeader('Last-Modified', date('r'));
+        $response->setHeader('Accept-Ranges', 'bytes');
+        $response->setHeader('Content-Length', strlen($content));
+        $response->setHeader('Content-type', $contentType);
+        $response->setBody($content);
+        $response->sendResponse();
+        die;
+    }
 }
