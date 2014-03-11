71f1be64a6d92e67aaf83bafc3ab87b6f679b112
Bartosz G <bartosz.wsnyc@gmail.com>
2014-03-11 20:32:16 +0100

    [Ticket #180] Styled the cart page

---
+++ app/code/local/Wsnyc/CartCouponCodeMessages/Helper/Data.php
@@ -0,0 +1,5 @@
+<?php
+class Wsnyc_CartCouponCodeMessages_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}
+	 
---
+++ app/code/local/Wsnyc/CartCouponCodeMessages/controllers/Checkout/CartController.php
@@ -0,0 +1,114 @@
+<?php
+
+/**
+ * Shopping cart controller
+ */
+require_once(BP . DS . 'app' . DS . 'code' . DS . 'core' . DS . 'Mage' . DS . 'Checkout' . DS . 'controllers' . DS . 'CartController.php');
+
+class Wsnyc_CartCouponCodeMessages_Checkout_CartController extends Mage_Checkout_CartController {
+    public function indexAction() {
+        $cart = $this->_getCart();
+        if ($cart->getQuote()->getItemsCount()) {
+            $cart->init();
+            $cart->save();
+
+            if (!$this->_getQuote()->validateMinimumAmount()) {
+                $minimumAmount = Mage::app()->getLocale()->currency(Mage::app()->getStore()->getCurrentCurrencyCode())
+                    ->toCurrency(Mage::getStoreConfig('sales/minimum_order/amount'));
+
+                $warning = Mage::getStoreConfig('sales/minimum_order/description')
+                    ? Mage::getStoreConfig('sales/minimum_order/description')
+                    : Mage::helper('checkout')->__('Minimum order amount is %s', $minimumAmount);
+
+                $cart->getCheckoutSession()->addNotice($warning);
+            }
+        }
+
+        // Compose array of messages to add
+        $messages = array();
+        foreach ($cart->getQuote()->getMessages() as $message) {
+            if ($message) {
+                // Escape HTML entities in quote message to prevent XSS
+                $message->setCode(Mage::helper('core')->escapeHtml($message->getCode()));
+                $messages[] = $message;
+            }
+        }
+        $cart->getCheckoutSession()->addUniqueMessages($messages);
+        $allMessages = Mage::getSingleton('checkout/session')->getMessages()->getItems();
+        foreach ($allMessages as $singleMessage) {
+            if (strpos(strtolower($singleMessage->getText()), 'coupon') !== false || 
+                strpos(strtolower($singleMessage->getText()), 'the code has restrictions') !== false) {
+                    $couponMessage = $singleMessage->getText();
+            }
+        }
+        /**
+         * if customer enteres shopping cart we should mark quote
+         * as modified bc he can has checkout page in another window.
+         */
+        $this->_getSession()->setCartWasUpdated(true);
+
+        Varien_Profiler::start(__METHOD__ . 'cart_display');
+        $this
+            ->loadLayout()
+            ->_initLayoutMessages('checkout/session')
+            ->_initLayoutMessages('catalog/session')
+            ->getLayout()->getBlock('head')->setTitle($this->__('Shopping Cart'));
+        if(isset($couponMessage)) {
+            $couponBlock = $this->getLayout()->getBlock('checkout.cart.coupon');
+            $couponBlock->setCouponMessages($couponMessage);
+        }
+        $this->renderLayout();
+        Varien_Profiler::stop(__METHOD__ . 'cart_display');
+    }
+    
+    public function couponPostAction() {
+        /**
+         * No reason continue with empty shopping cart
+         */
+        if (!$this->_getCart()->getQuote()->getItemsCount()) {
+            $this->_goBack();
+            return;
+        }
+
+        $couponCode = (string) $this->getRequest()->getParam('coupon_code');
+        if ($this->getRequest()->getParam('remove') == 1) {
+            $couponCode = '';
+        }
+        $oldCouponCode = $this->_getQuote()->getCouponCode();
+
+        if (!strlen($couponCode) && !strlen($oldCouponCode)) {
+            $this->_goBack();
+            return;
+        }
+
+        try {
+            $this->_getQuote()->getShippingAddress()->setCollectShippingRates(true);
+            $this->_getQuote()->setCouponCode(strlen($couponCode) ? $couponCode : '')
+                ->collectTotals()
+                ->save();
+
+            if (strlen($couponCode)) {
+                if ($couponCode == $this->_getQuote()->getCouponCode()) {
+                    $this->_getSession()->addSuccess(
+                        $this->__('Coupon code "%s" was applied.', Mage::helper('core')->htmlEscape($couponCode))
+                    );
+                }
+                else {
+                    $this->_getSession()->addError(
+                        $this->__('You have either entered an invalid code or the code has restrictions for certain items in your cart.')
+                    );
+                }
+            } else {
+                $this->_getSession()->addSuccess($this->__('Coupon code was canceled.'));
+            }
+
+        } catch (Mage_Core_Exception $e) {
+            $this->_getSession()->addError($e->getMessage());
+        } catch (Exception $e) {
+            $this->_getSession()->addError($this->__('Cannot apply the coupon code.'));
+            Mage::logException($e);
+        }
+
+        $this->_goBack();
+    }
+}
