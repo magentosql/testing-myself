0fac2b3a3ae24fe540ed137e5fa2c0e22cea1eec
Bartosz G <bartosz.wsnyc@gmail.com>
2014-03-14 23:56:21 +0100

    [Ticket #147] Finished mailchimp newsletter modal box

---
+++ app/code/local/Wsnyc/NewsletterModal/Block/Newsletter.php
@@ -0,0 +1,8 @@
+<?php
+class Wsnyc_NewsletterModal_Block_Newsletter extends Mage_Core_Block_Template
+{
+    protected function _construct()
+    {
+        $this->setTemplate('wsnyc/newslettermodal/newslettermodal.phtml');
+    }
+}
---
+++ app/code/local/Wsnyc/NewsletterModal/Helper/Data.php
@@ -0,0 +1,5 @@
+<?php
+
+class Wsnyc_NewsletterModal_Helper_Data extends Mage_Core_Helper_Abstract {
+    
+}
