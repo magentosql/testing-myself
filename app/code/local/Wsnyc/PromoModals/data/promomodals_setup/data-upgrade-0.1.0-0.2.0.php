5c41249f2dbf9611609f83d419f35a8e43e6f872
Bartosz G <bartosz.wsnyc@gmail.com>
2014-05-26 11:32:48 +0200

    Refs #198: Created a new promotional modal box and refactored the PromoModals module

---
+++ app/code/local/Wsnyc/PromoModals/data/promomodals_setup/data-upgrade-0.1.0-0.2.0.php
@@ -0,0 +1,18 @@
+<?php
+$installer = $this;
+$pageContent = <<< EOT
+<p>Visit <a href="https://www.facebook.com/thelaundressnyc" target="_blank">Facebook</a> to enter between June 2, 2014 at 12:00am EST and June 30, 2014 at 11:59pm EST.</p>
+<p>Voting period begins July 1, 2014 at 12:00am EST and ends July 31, 2014 at 11:59pm EST.</p>
+<p>Two lucky winners will be selected from the top 20 entries based on fan votes. Each winner will receive $500 of FREE pre-selected Laundress products.</p>
+EOT;
+$page = Mage::getModel('cms/block')->load('modal-photocontest_june_2014','identifier');
+if($page->isObjectNew()) {
+    $page->setIdentifier('modal-photocontest_june_2014')
+            ->setTitle('The Laundress Wash & Win Photo Contest')
+            ->setIsActive('1')
+            ->setStores(array('0'));
+} 
+$page->setContent($pageContent)
+        ->setTitle('The Laundress Wash & Win Photo Contest')
+        ->save();
+	 
