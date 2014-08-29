a35c83466dfb836dca4bd4b3e014de365553dce0
Artur J <artur@websolutionsnyc.com>
2014-08-29 13:59:35 +0200

    Refs #219 - add sitemap options for ask the laundress items

--- app/code/local/Wsnyc/QuestionsAnswers/Model/Observer.php
+++ app/code/local/Wsnyc/QuestionsAnswers/Model/Observer.php
@@ -7,8 +7,8 @@ class Wsnyc_QuestionsAnswers_Model_Observer {
         $io = $observer->getEvent()->getIo();
         $date    = Mage::getSingleton('core/date')->gmtDate('Y-m-d');
         
-        $changefreq = (string)Mage::getStoreConfig('sitemap/page/changefreq', $storeId);
-        $priority   = (string)Mage::getStoreConfig('sitemap/page/priority', $storeId);
+        $changefreq = (string)Mage::getStoreConfig('sitemap/asklaundress/changefreq', $storeId);
+        $priority   = (string)Mage::getStoreConfig('sitemap/asklaundress/priority', $storeId);
         $collection = Mage::getResourceModel('wsnyc_questionsanswers/category_collection');
         $collection->addFieldToFilter('parent_id',array('eq'=>0));
         foreach ($collection as $item) {
