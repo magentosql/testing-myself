9d5231edc44d4ad7e7a7a7a4e93c41fc738ce370
Bartosz G <bartosz@earphonesolutions.com>
2014-11-05 18:48:01 +0100

    Refs #239: Disabled Shipping and Shipping Method in the Checkout Progress for Distributors

--- app/code/local/Wsnyc/MultipleWebsites/Helper/Data.php
+++ app/code/local/Wsnyc/MultipleWebsites/Helper/Data.php
@@ -9,5 +9,15 @@ class Wsnyc_MultipleWebsites_Helper_Data extends Mage_Core_Helper_Abstract {
             return 1;
         }        
     }
+    
+    public function getCustomerGroupType() {
+        if (Mage::getSingleton('customer/session')->isLoggedIn()) {
+            return Mage::getModel('customer/group')->load(Mage::getSingleton('customer/session')->getCustomer()->getGroupId())->getGroupType();
+        } elseif (Mage::app()->getWebsite()->getId() === '0') {
+            return Mage::getModel('customer/group')->load(Mage::getSingleton('adminhtml/session_quote')->getQuote()->getCustomer()->getGroupId())->getGroupType();
+        } else {
+            return 0;
+        }        
+    }
 }
 	 
