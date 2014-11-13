5e1fc164c60b9667efaced196dd9b55e90285577
Bartosz G <bartosz@websolutionsnyc.com>
2014-11-13 05:09:20 +0100

    Refs #239: Bill to Account Number shipping method is now allowed only to selected customer groups

--- app/code/local/Wsnyc/BillToAccountNumber/Helper/Data.php
+++ app/code/local/Wsnyc/BillToAccountNumber/Helper/Data.php
@@ -1,5 +1,23 @@
 <?php
-class Wsnyc_BillToAccountNumber_Helper_Data extends Mage_Core_Helper_Abstract
-{
+
+class Wsnyc_BillToAccountNumber_Helper_Data extends Mage_Core_Helper_Abstract {
+    public function checkIfShippingMethodIsAllowed() {
+        if (Mage::getSingleton('customer/session')->isLoggedIn()) {
+            $customerGroupId = Mage::getSingleton('customer/session')->getCustomer()->getGroupId();
+        }
+        if(Mage::app()->getRequest()->getModuleName() == 'thelaund_admin' && Mage::getSingleton('adminhtml/session_quote')) {
+            $store = Mage::getSingleton('adminhtml/session_quote')->getStoreId();
+            $customer = Mage::getSingleton('adminhtml/session_quote')->getQuote()->getCustomer();
+            if($customer) {
+                $customerGroupId = $customer->getGroupId();
+            }
+        } else {
+            $store = null;
+        }
+        $availableForGroups = explode(',', Mage::getStoreConfig('carriers/billtoaccountnumber/customergroups', $store));
+        if ($customerGroupId && in_array($customerGroupId, $availableForGroups)) {
+            return true; 
+        }
+        return false; 
+    }
 }
-	 
--- app/code/local/Wsnyc/BillToAccountNumber/Model/Carrier/Billtoaccountnumber.php
+++ app/code/local/Wsnyc/BillToAccountNumber/Model/Carrier/Billtoaccountnumber.php
@@ -1,36 +1,28 @@
-<?php  
-    class Wsnyc_BillToAccountNumber_Model_Carrier_Billtoaccountnumber     
-		extends Mage_Shipping_Model_Carrier_Abstract
-		implements Mage_Shipping_Model_Carrier_Interface
-	{  
-        protected $_code = 'billtoaccountnumber';  
-      
-        /** 
-        * Collect rates for this shipping method based on information in $request 
-        * 
-        * @param Mage_Shipping_Model_Rate_Request $data 
-        * @return Mage_Shipping_Model_Rate_Result 
-        */  
-        public function collectRates(Mage_Shipping_Model_Rate_Request $request){  
-            $result = Mage::getModel('shipping/rate_result');  
-            $method = Mage::getModel('shipping/rate_result_method');  
-            $method->setCarrier($this->_code);  
-            $method->setCarrierTitle($this->getConfigData('title'));
-            $method->setMethod($this->_code);  
-            $method->setMethodTitle($this->getConfigData('name'));
-		    $method->setPrice('0.00');
-			$method->setCost('0.00');
-            $result->append($method);  
-            return $result;  
-        }  
+<?php
 
-		/**
-		 * Get allowed shipping methods
-		 *
-		 * @return array
-		 */
-		public function getAllowedMethods()
-		{
-			return array($this->_code=>$this->getConfigData('name'));
-		}
-    }  
+class Wsnyc_BillToAccountNumber_Model_Carrier_Billtoaccountnumber extends Mage_Shipping_Model_Carrier_Abstract implements Mage_Shipping_Model_Carrier_Interface {
+
+    protected $_code = 'billtoaccountnumber';
+
+    public function collectRates(Mage_Shipping_Model_Rate_Request $request) {
+        $result = Mage::getModel('shipping/rate_result');
+        if(Mage::helper('billtoaccountnumber')->checkIfShippingMethodIsAllowed()) {
+            $method = Mage::getModel('shipping/rate_result_method')
+                ->setCarrier($this->_code)
+                ->setCarrierTitle($this->getConfigData('title'))
+                ->setMethod($this->_code)
+                ->setMethodTitle($this->getConfigData('name'))
+                ->setPrice('0.00')
+                ->setCost('0.00');
+            $result->append($method);
+        }
+        return $result;
+    }
+
+    public function getAllowedMethods() {
+        if(Mage::helper('billtoaccountnumber')->checkIfShippingMethodIsAllowed()) {
+            return array($this->_code => $this->getConfigData('name'));
+        }
+    }
+
+}
