14ac5348f16f79d8d8b17f0d6a6c340d24a23526
Artur J <artur@websolutionsnyc.com>
2014-12-03 14:16:24 +0100

    Refs #278 - update seo subfooter

--- app/code/local/Wsnyc/SeoSubfooter/Helper/Data.php
+++ app/code/local/Wsnyc/SeoSubfooter/Helper/Data.php
@@ -2,5 +2,8 @@
 
 class Wsnyc_SeoSubfooter_Helper_Data extends Mage_Core_Helper_Abstract {
     
-    
+    public function getCmsPageBodyClass() {
+        $page = Mage::getSingleton('cms/page');
+        return $page->getSeosubfooterLink() ? 'seosubfooter-page' : null;
+    }
 }
