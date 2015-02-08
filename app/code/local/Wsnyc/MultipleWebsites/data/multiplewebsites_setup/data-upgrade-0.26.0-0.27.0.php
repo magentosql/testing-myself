b2433f975a25d0833d18642ec33c2a51d2a63d21
Bartosz G <bartosz@websolutionsnyc.com>
2015-02-08 12:28:06 +0100

    Refs #310: Changed the Terms & Conditions page

--- app/code/local/Wsnyc/MultipleWebsites/data/multiplewebsites_setup/data-upgrade-0.26.0-0.27.0.php
+++ app/code/local/Wsnyc/MultipleWebsites/data/multiplewebsites_setup/data-upgrade-0.26.0-0.27.0.php
@@ -2,9 +2,11 @@
 
 $installer = $this;
 
+$retailStore = Mage::getModel('core/store')->load('default','code');
+
 $cmsPage = Mage::getModel('cms/page')->getCollection()
         ->addFieldToFilter('identifier', array('eq' => 'customer-service/terms'))
-        ->addStoreFilter(Mage::getModel('core/store')->load('default','code'))
+        ->addStoreFilter($retailStore)
         ->load()
         ->getFirstItem();
         
