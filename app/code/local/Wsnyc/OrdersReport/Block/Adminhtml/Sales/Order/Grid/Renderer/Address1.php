19c413cb18e338f385c968dfa6e2f7559cca41c8
Adam Hańczewski <ahanczewski@intah.pl>
2015-04-30 23:38:14 +0200

    Refs #371 - 30DayClean Magento Reporting

--- app/code/local/Wsnyc/OrdersReport/Block/Adminhtml/Sales/Order/Grid/Renderer/Address1.php
+++ app/code/local/Wsnyc/OrdersReport/Block/Adminhtml/Sales/Order/Grid/Renderer/Address1.php
@@ -4,8 +4,28 @@ class Wsnyc_OrdersReport_Block_Adminhtml_Sales_Order_Grid_Renderer_Address1 exte
 {
     protected function _getValue(Varien_Object $row)
     {
-        $street = explode("\n", $row['street']);
-        return $street[0];
+        return $this->getStreet(1, $row['street']);
     }
 
+    /**
+     * get address street
+     *
+     * @param   int $line address line index
+     * @return  string
+     */
+    public function getStreet($line=0, $street)
+    {
+        if (-1 === $line) {
+            return $street;
+        } else {
+            $arr = is_array($street) ? $street : explode("\n", $street);
+            if (0 === $line || $line === null) {
+                return $arr;
+            } elseif (isset($arr[$line-1])) {
+                return $arr[$line-1];
+            } else {
+                return '';
+            }
+        }
+    }
 }
