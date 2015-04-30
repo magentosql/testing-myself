e931634167faeba9057fa3bc161e331eac9be2ab
Artur J <artur@websolutionsnyc.com>
2015-04-30 10:07:54 +0200

    Refs #380 -fix google remarketing pixel

--- app/code/local/Wsnyc/GoogleRemarketing/Model/Observer.php
+++ app/code/local/Wsnyc/GoogleRemarketing/Model/Observer.php
@@ -1,10 +1,11 @@
 <?php
 
 class Wsnyc_GoogleRemarketing_Model_Observer {
-  
-  public function saveLastOrderId($observer) {
-    $order = $observer->getEvent()->getOrderIds();    
-    Mage::app()->getLayout()->getBlock('remarketing.checkout')->setOrdersId($order);
-    
-  }
+
+    public function saveLastOrderId($observer) {
+        $order = $observer->getEvent()->getOrderIds();
+        if(Mage::app()->getLayout()->getBlock('remarketing.checkout')) {
+            Mage::app()->getLayout()->getBlock('remarketing.checkout')->setOrdersId($order);
+        }
+    }
 }
