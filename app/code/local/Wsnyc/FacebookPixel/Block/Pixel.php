2cc801540544702be439fb8cc0e915d609e07a55
Artur J <artur@websolutionsnyc.com>
2015-05-21 10:56:46 +0200

    Refs #396 - fix fb conversion pixel

--- app/code/local/Wsnyc/FacebookPixel/Block/Pixel.php
+++ app/code/local/Wsnyc/FacebookPixel/Block/Pixel.php
@@ -5,6 +5,7 @@ class Wsnyc_FacebookPixel_Block_Pixel extends Mage_Core_Block_Template {
     const XML_ENABLED = 'facebook_pixel/general/enabled';
     const XML_PIXEL_ID = 'facebook_pixel/general/pixel_id';
     const XML_CONVERSION_TRACKING = 'facebook_pixel/general/track_conversion';
+    const XML_CONVERSION_PIXEL_ID = 'facebook_pixel/general/conversion_pixel_id';
 
     public function isEnabled() {
         return Mage::getStoreConfig(self::XML_ENABLED);
@@ -16,5 +17,9 @@ class Wsnyc_FacebookPixel_Block_Pixel extends Mage_Core_Block_Template {
 
     public function getPixelId() {
         return Mage::getStoreConfig(self::XML_PIXEL_ID);
+    }
+
+    public function getConversionPixelId() {
+        return Mage::getStoreConfig(self::XML_CONVERSION_PIXEL_ID);
     }
 }
