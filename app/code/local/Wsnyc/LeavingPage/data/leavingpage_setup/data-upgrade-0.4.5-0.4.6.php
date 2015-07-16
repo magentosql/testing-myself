038a3f2f58372cbdc717a94b10267b6678907853
Artur J <artur@websolutionsnyc.com>
2015-07-16 11:19:21 +0200

    Refs #419 - update leaving page modal

---
+++ app/code/local/Wsnyc/LeavingPage/data/leavingpage_setup/data-upgrade-0.4.5-0.4.6.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @var Mage_Core_Model_Resource_Setup $installer
+ */
+$installer = $this;
+$installer->startSetup();
+$blockContent = <<< EOT
+<h3>Stay a while longer...</h3>
+<p>We promise you'll love our products.<br />Shop with 10% off your order using code:</p>
+EOT;
+
+Mage::getModel('cms/block')->load('pageleave-modal','identifier')
+    ->setContent($blockContent)
+    ->save();
+
+$installer->endSetup();
