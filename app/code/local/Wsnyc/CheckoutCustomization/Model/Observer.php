e9e3995a7a0174d5531d45fc61eaeb7a913aa412
Artur J <artur@websolutionsnyc.com>
2015-07-17 11:55:22 +0200

    Refs #415 - check if current customer is eligible for the store

--- app/code/local/Wsnyc/CheckoutCustomization/Model/Observer.php
+++ app/code/local/Wsnyc/CheckoutCustomization/Model/Observer.php
@@ -92,4 +92,44 @@ class Wsnyc_CheckoutCustomization_Model_Observer {
             $order->setOnestepcheckoutGiftwrapType($giftwrapType);
         }
     }
+
+    /**
+     * Check if quote customer is correct
+     *
+     * @param Varien_Event_Observer $observer
+     */
+    public function checkQuoteCustomer(Varien_Event_Observer $observer)
+    {
+        $store = Mage::app()->getStore();
+        $quote = Mage::getSingleton('checkout/session')->getQuote();
+        if (!Mage::getSingleton('customer/session')->getCustomer()->isInStore($store)) {
+            $this->_fixQuoteCustomer($quote);
+        }
+        elseif ($quote->getCustomerGroupId() > 1) {
+            $quoteCustomer = Mage::getModel('customer/customer')->load($quote->getCustomerId());
+            if (!$quoteCustomer->isInStore($store)) {
+                $this->_fixQuoteCustomer($quote);
+            }
+        }
+    }
+
+    /**
+     * Fix quote customer association; Logout incorrect customer
+     *
+     * @param Mage_Sales_Model_Quote $quote
+     */
+    protected function _fixQuoteCustomer(Mage_Sales_Model_Quote $quote)
+    {
+        Mage::log('testme');
+        Mage::getSingleton('customer/session')->logout();
+        $customer = Mage::getModel('customer/customer')->setWebsiteId(Mage::app()->getStore()->getWebsiteId());
+        $quote->assignCustomer($customer)
+                ->setCustomerGroupId(null)
+                ->setCustomerId(null)
+                ->save();
+        Mage::getSingleton('customer/session')->setCustomer($customer);
+        if (Mage::helper('persistent')->isEnabled(Mage::app()->getStore())) {
+            Mage::helper('persistent/session')->getSession()->setCustomerId(null)->save();
+        }
+    }
 }
